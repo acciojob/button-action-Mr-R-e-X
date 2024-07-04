fca8c07c20097cc9aa87c866714a97f525e9c6c6
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-04 17:44:43 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = (props) => {
-
+  const [data, setData] = useState(false);
+  function changeData() {
+    setData(!data);
+  }
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {data ? (
+        <p id="para">
+          "Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        ""
+      )}
+      <button id="click" onClick={() => changeData()}>
+        {data ? "hide" : "show"}
+      </button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
